e6812b2edfb380f195eceefb94fe566772d3307d
ViktorAtanasov42 <viki@mebelina.com>
2022-03-12 18:13:10 +0200

    create, edit and delete posts

---
+++ WebAppForumDemo/Controllers/PostController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppForumDemo.Models;
+using WebAppForumDemo.Services;
+
+namespace WebAppForumDemo.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly PostService postService;
+
+        public PostController(PostService postService)
+        {
+            this.postService = postService;
+        }
+
+        public IActionResult Index()
+        {
+            List<Post> posts = postService.GetAll();
+
+            return View(posts);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+         
+        [HttpPost]
+        public IActionResult Create(string title, string content)
+        {
+            postService.Create(title, content);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Post post = postService.GetById(id);
+            return View(post);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, string title, string content)
+        {
+            postService.Edit(id, title, content);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            Post post = postService.GetById(id);
+            return View(post);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteConfirm(int id)
+        {
+            postService.Delete(id);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
